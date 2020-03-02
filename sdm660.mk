6527df9c1c8e586ee6980a31a1700d8778cd870c
SayanAcharya <rowdysayan99@gmail.com>
2020-03-02 04:51:44 +0000

    sdm660-common: Import antradio-library
    Signed-off-by: SayanAcharya <rowdysayan99@gmail.com>

--- sdm660.mk
+++ sdm660.mk
@@ -124,7 +124,7 @@ PRODUCT_PACKAGES += \
 
 # ANT Permission
 PRODUCT_COPY_FILES += \
-    external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.dsi.ant.antradio_library.xml
+    $(COMMON_PATH)/antradio-library/com.dsi.ant.antradio_library.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.dsi.ant.antradio_library.xml
 
 # AuthSecret
 PRODUCT_PACKAGES += \
@@ -143,6 +143,7 @@ PRODUCT_PACKAGES += \
     libhdmiedid \
     libhfp \
     libldacBT_dec \
+    libbtconfigstore \
     libsndmonitor \
     vendor.qti.hardware.bluetooth_audio@2.0.vendor
 
