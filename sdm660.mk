b8b679c3ad9465a898e29e43c2d6fddcce691dd4
Keerten <keertenk@gmail.com>
2020-04-19 15:20:33 +0530

    sdm660-common: Refactor ANT for CAF

--- sdm660.mk
+++ sdm660.mk
@@ -136,13 +136,7 @@ PRODUCT_COPY_FILES += \
 
 # ANT+
 PRODUCT_PACKAGES += \
-    AntHalService \
-    antradio_app \
-    com.dsi.ant.antradio_library \
-    libantradio
-
-PRODUCT_COPY_FILES += \
-    external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.dsi.ant.antradio_library.xml
+    AntHalService
 
 # Biometrics
 PRODUCT_PACKAGES += \
